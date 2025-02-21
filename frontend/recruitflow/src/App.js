2fed725c17971beadeb898f04fda52fc725d2f65
Le Quoc Khanh <khanhlqhe172534@fpt.edu.vn>
2025-02-21 09:27:21 +0700

    add fe for offer management

--- frontend/recruitflow/src/App.js
+++ frontend/recruitflow/src/App.js
@@ -7,6 +7,9 @@ import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
 import NoAccess from './pages/NoAccess';
 import UserManagement from './pages/UserManagement';
+import OfferManagement from './pages/OfferManagement';
+import CandidateManagerment from './pages/CandidateManagement';
+import CandidateManagement from './pages/CandidateManagement';
 
 function ProtectedRoute({ isAuthenticated, children }) {
   return isAuthenticated ? children : <Navigate to="/no-access" />;
@@ -67,6 +70,22 @@ function App() {
             element={
               <ProtectedRoute isAuthenticated={isAuthenticated}>
                 <UserManagement />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="offer"
+            element={
+              <ProtectedRoute isAuthenticated={isAuthenticated}>
+                <OfferManagement />
+              </ProtectedRoute>
+            }
+          />
+           <Route
+            path="candidate"
+            element={
+              <ProtectedRoute isAuthenticated={isAuthenticated}>
+                <CandidateManagement />
               </ProtectedRoute>
             }
           />
